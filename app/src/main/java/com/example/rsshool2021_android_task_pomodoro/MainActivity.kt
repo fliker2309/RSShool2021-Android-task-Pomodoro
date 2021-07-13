40b82299a8b34c912aa66314d8b0e4fea79f0fc1
Evgenii <60140529+fliker2309@users.noreply.github.com>
2021-07-13 15:37:12 +0300

    add:StopWatchViewHolder.kt

--- app/src/main/java/com/example/rsshool2021_android_task_pomodoro/MainActivity.kt
+++ app/src/main/java/com/example/rsshool2021_android_task_pomodoro/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.rsshool2021_android_task_pomodoro
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.rsshool2021_android_task_pomodoro.databinding.ActivityMainBinding
+import com.example.rsshool2021_android_task_pomodoro.model.Stopwatch
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val stopwatches = mutableListOf<Stopwatch>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/example/rsshool2021_android_task_pomodoro/ui/StopwatchViewHolder.kt
@@ -0,0 +1,37 @@
+package com.example.rsshool2021_android_task_pomodoro.ui
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.rsshool2021_android_task_pomodoro.databinding.StopwatchItemBinding
+import com.example.rsshool2021_android_task_pomodoro.model.Stopwatch
+
+class StopwatchViewHolder(private val binding: StopwatchItemBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+
+
+    fun bind(stopwatch: Stopwatch) {
+        binding.stopwatchTimer.text = stopwatch.currentMs.displayTime()
+    }
+
+    private fun Long.displayTime(): String {
+        if (this <= 0L) return START_TIME
+
+        val h = this / 1000 / 3600
+        val m = this / 1000 % 3600 / 60
+        val s = this / 1000 % 60
+
+        return "${displaySlot(h)}:${displaySlot(m)}:${displaySlot(s)}"
+    }
+
+    private fun displaySlot(count: Long): String {
+        return if (count / 10L > 0) {
+            "$count"
+        } else {
+            "0$count"
+        }
+    }
+
+
+    private companion object {
+        private const val START_TIME = "00:00:00"
+    }
+}
