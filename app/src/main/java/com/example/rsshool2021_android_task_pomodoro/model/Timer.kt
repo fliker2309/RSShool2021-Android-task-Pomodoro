fc5a56271dbdd16b342479beb778b8321ac6f3fa
Zhenya <evgeniikapchik@gmail.com>
2021-07-18 14:12:12 +0300

    fix: dataClass Timer.kt

--- app/src/main/java/com/example/rsshool2021_android_task_pomodoro/model/Timer.kt
+++ app/src/main/java/com/example/rsshool2021_android_task_pomodoro/model/Timer.kt
@@ -3,5 +3,7 @@ package com.example.rsshool2021_android_task_pomodoro.model
 data class Timer(
     val id: Int,
     var currentMs: Long,
-    var isStarted: Boolean
+    var startMs: Long,
+    var isStarted: Boolean = false,
+    var isFinished: Boolean = false
 )
