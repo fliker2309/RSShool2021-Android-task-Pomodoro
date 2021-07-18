77d3e6e61a91cecec09d3f0f146f19afad5e9035
Zhenya <evgeniikapchik@gmail.com>
2021-07-18 13:40:06 +0300

    add:attrs.xml

--- app/src/main/java/com/example/rsshool2021_android_task_pomodoro/CustomView.kt
+++ app/src/main/java/com/example/rsshool2021_android_task_pomodoro/CustomView.kt
@@ -1,7 +1,9 @@
 package com.example.rsshool2021_android_task_pomodoro
 
 import android.content.Context
+import android.graphics.Paint
 import android.util.AttributeSet
+import android.view.Gravity.FILL
 import android.view.View
 import androidx.annotation.AttrRes
 
@@ -10,4 +12,27 @@ class CustomView @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     @AttrRes defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
+
+    private val periodMs = 0L
+    private val currentMs = 0L
+    private var color = 0
+    private var style = FILL
+    private val paint = Paint()
+
+    init {
+        if (attrs != null){
+            val styledAttrs = context.theme.obtainStyledAttributes(
+                attrs,
+                R.styleable.CustomView,
+                defStyleAttr,
+                0
+            )
+        }
+
+    }
+
+
+
 }
+
+
